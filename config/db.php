7957667c59d376e242fbc7aa16260454ff001536
Sugeng Sulistiyawan <sugeng.sulistiyawan@gmail.com>
2023-04-10 17:05:52 +0700

    Test

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=yii',
     'username' => 'root',
-    'password' => '',
+    'password' => 'yii',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
